05255d9f71e8691c64be49a473d2d782ec2b6e6f
DeltaLima <marcus@deltalima.org>
2022-08-21 22:12:06 +0000

    „install-xash3ds.sh“ ändern

--- install-xash3ds.sh
+++ install-xash3ds.sh
@@ -9,21 +9,19 @@ metamod_url="https://github.com/mittorn/metamod-p/releases/download/1/metamod.so
 amxmod_url="http://www.amxmodx.org/release/amxmodx-$amxmod_version-base-linux.tar.gz"
 jk_botti_url="http://koti.kapsi.fi/jukivili/web/jk_botti/jk_botti-$jk_botti_version-release.tar.xz"
 
-if [ ! -z "$1" ]
-then
-    if [ "$1" == "client" ]
-    then
-        CLIENT=true
-        PACKAGES="build-essential gcc-multilib g++-multilib python libsdl2-dev:i386 libfontconfig-dev:i386 libfreetype6-dev:i386"
-        WAF_OPTION=""
-        export PKG_CONFIG_PATH=/usr/lib/i386-linux-gnu/pkgconfig
-    else 
-        CLIENT=false
-        XASHDS_PORT=27015
-        CLIENT=false
-        PACKAGES="build-essential  ca-certificates  cmake  curl  git  gnupg2 g++-multilib lib32gcc1-s1 libstdc++6:i386 python unzip xz-utils zip"
-        WAF_OPTION="-d"
-    fi   
+
+if [ "$1" == "client" ]
+  then
+      CLIENT=true
+      PACKAGES="build-essential gcc-multilib g++-multilib python libsdl2-dev:i386 libfontconfig-dev:i386 libfreetype6-dev:i386"
+      WAF_OPTION=""
+      export PKG_CONFIG_PATH=/usr/lib/i386-linux-gnu/pkgconfig
+  else 
+      CLIENT=false
+      XASHDS_PORT=27015
+      CLIENT=false
+      PACKAGES="build-essential  ca-certificates  cmake  curl  git  gnupg2 g++-multilib lib32gcc1-s1 libstdc++6:i386 python unzip xz-utils zip"
+      WAF_OPTION="-d"
 fi
 
 XASH3D_BASEDIR=$(pwd)/build
